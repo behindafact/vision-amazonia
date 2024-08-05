1a7e276b1b1e5ce12a39c72f7b8569241f2e0faa
behindafact <julianjimenez2811@gmail.com>
2024-08-05 10:17:10 -0500

    agregué funcionalidad para visualizar el resultado ML de cuencas

--- streamlitMaps.py
+++ streamlitMaps.py
@@ -23,15 +23,19 @@ df = pd.DataFrame({
 ]
 })
 
+df2 = pd.read_csv('cuencas.csv')
 
 with open("proyecto.geojson",'r', encoding='utf-8') as f:
     gj = geojson.load(f)
+
+with open("cuencas_pred.geojson",'r', encoding='utf-8') as f:
+    gj2 = geojson.load(f)
 
 st.header('VISOR AMAZONIA')
 #dfRestaurantes= pd.read_csv('googlemaps_comida china.csv')
 ##dfRestaurantes['review_count']=dfRestaurantes['review_count'].fillna(1)
 
-tab2,tab4=st.tabs(['Mapa deforesacion proyecto Guainia' ,'Datos'])
+tab2,tab4=st.tabs(['Mapa deforesacion proyecto Guainia' ,'Random Forestt Classfier for Watershed'])
 with tab2:
     #df = px.data.gapminder().query("year==2007")    
     fig = px.choropleth_mapbox(data_frame=df,geojson=gj,
@@ -39,7 +43,6 @@ with tab2:
                         color="DEFORESTATION 2023",
                         center={"lat": 3.363650, "lon": -69.091847},
                         featureidkey='properties.COMUNIDAD',
-                        #hover_name="country", # column to add to hover information
                         mapbox_style="carto-positron",
                         color_continuous_scale=px.colors.sequential.Plasma,
                         zoom=6.5,
@@ -48,6 +51,11 @@ with tab2:
     st.plotly_chart(fig,use_container_width=True)
     #st.dataframe(df)
 
-#with tab4:
-    #st.dataframe(dfRestaurantes,use_container_width=True)
-
+with tab4:
+    fig = px.choropleth_mapbox(df2, geojson=gj2, color="Prediccion",
+                           locations="Field1", featureidkey="properties.Field1",
+                           center={"lat": 6.276656, "lon": -75.587327},
+                           mapbox_style="carto-positron", zoom=8,
+                           width= 800,
+                            height= 800)
+    st.plotly_chart(fig,use_container_width=True)
